d101516aa258ab957056488ffd95dcb271b2e1f3
asem <nehalo@bitbucket.org>
2015-12-13 23:04:22 +0200

    In VirtualGPU class, each GPU instantiated will have some parameters of random values, assuming the GPU that process a volume of (x*y*z) will have a quasai-linear processing time represented by: a*(xyz) + b*(xy) + c*(xz) + d*(yz) + e*(x) + f*(y) + g*(z)

--- VirtualGPU.cpp
+++ VirtualGPU.cpp
@@ -1,7 +1,44 @@
 #include "VirtualGPU.h"
+#include <stdlib.h>
 
 VirtualGPU::VirtualGPU()
 {
 
 }
 
+double VirtualGPU::volumeRenderingTime( VirtualVolume &volume_ )
+{
+    return  volumeProcessingTime_( volume_ ) *
+            VirtualExperiment::processScale( VirtualExperiment::ProcessOrder::Rendering );
+}
+
+double VirtualGPU::volumeProcessingTime_( VirtualVolume &volume_ )
+{
+    //non-linear
+    return  parameters_.a * volume_.dim()[0] * volume_.dim()[1] * volume_.dim()[2] +
+            parameters_.b * volume_.dim()[0] * volume_.dim()[1] +
+            parameters_.c * volume_.dim()[0] * volume_.dim()[2] +
+            parameters_.d * volume_.dim()[1] * volume_.dim()[2] +
+            parameters_.e * volume_.dim()[0] +
+            parameters_.f * volume_.dim()[1] +
+            parameters_.g * volume_.dim()[2] ;
+}
+
+
+GPUParameters::GPUParameters()
+{
+    // a*(xyz) OR a*volume ... highest order. Should be with highest correlation with time
+
+    a = drand48();
+
+    // b,c,d ... 2nd order.
+    b= drand48()/2;
+    c= drand48()/2;
+    d= drand48()/2;
+
+    // e,f,g ... 1st order
+    e= drand48()/4;
+    f= drand48()/4;
+    g= drand48()/4;
+
+}
--- VirtualGPU.h
+++ VirtualGPU.h
@@ -1,11 +1,35 @@
 #ifndef VIRTUALGPU_H
 #define VIRTUALGPU_H
+#include <VirtualExperiment.h>
+#include <VirtualVolume.h>
 
+//GPU parameters: some heuristic quasai-linear equation to mimic the gpu processing time
+// a*(xyz) + b*(xy) + c*(xz) + d*(zy) + e*(x) + f*(y) + g*(z)
+struct GPUParameters
+{
+    GPUParameters();
+    double a;
+    double b;
+    double c;
+    double d;
+    double e;
+    double f;
+    double g;
+};
 
 class VirtualGPU
 {
 public:
     VirtualGPU();
+
+    double volumeRenderingTime( VirtualVolume &volume_) ;
+
+
+private:
+
+    double volumeProcessingTime_(VirtualVolume &volume_);
+
+    GPUParameters parameters_;
 };
 
 #endif // VIRTUALGPU_H
