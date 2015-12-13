50cc20bb11c51ff8533e80b19f1099d22fa405e1
asem <nehalo@bitbucket.org>
2015-12-13 23:01:12 +0200

    In this class three types of processes are simulated: Rendering; assigned processing scale of 10, Compression; has processing scale of 0.01, Compositing; has processing scale of 0.1

--- VirtualExperiment.cpp
+++ VirtualExperiment.cpp
@@ -1,7 +1,18 @@
 #include "VirtualExperiment.h"
+#include <math.h>
 
-VirtualExperiment::VirtualExperiment()
+VirtualExperiment::VirtualExperiment(const ProcessOrder processOrder)
+    : processOrder_( processOrder )
 {
 
 }
 
+double VirtualExperiment::processScale() const
+{
+    return pow( 10 , static_cast<double>(processOrder_ ) );
+}
+
+double VirtualExperiment::processScale(VirtualExperiment::ProcessOrder processOrder)
+{
+    return pow( 10 , static_cast<double>(processOrder ) );
+}
--- VirtualExperiment.h
+++ VirtualExperiment.h
@@ -2,10 +2,20 @@
 #define VIRTUALEXPERIMENT_H
 
 
+
 class VirtualExperiment
 {
 public:
-    VirtualExperiment();
+    enum class ProcessOrder : int  { Rendering = 0  , Compression = -2 , Compositing = -1 };
+
+    VirtualExperiment( const ProcessOrder processOrder );
+
+    double processScale() const ;
+
+    static double processScale( ProcessOrder processOrder ) ;
+private:
+
+    const ProcessOrder processOrder_ ;
 };
 
 #endif // VIRTUALEXPERIMENT_H
