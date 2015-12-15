35b6b0c92e7cf6b1d1f49e066fd99b9e64582432
asem <nehalo@bitbucket.org>
2015-12-16 00:05:18 +0200

    Provide class with run() method

--- TaskRender.cpp
+++ TaskRender.cpp
@@ -1,7 +1,52 @@
 #include "TaskRender.h"
 
-TaskRender::TaskRender()
+
+TaskRender::TaskRender( const VirtualNode &node,
+                        const TaskRender::RenderingTask renderingTask,
+                        const Transformation *transformation)
+    : node_( node ),
+      renderingTask_( renderingTask),
+      transformation_( transformation )
 {
+    if ( renderingTask == RenderingTask::ApplyTransformation &&
+         transformation == nullptr )
+    {
+        std::count << "Pass valid transformations!\n";
+        exit(EXIT_FAILURE);
+    }
 
 }
 
+const VirtualNode &TaskRender::node() const
+{
+    return node_;
+}
+
+TaskRender::RenderingTask TaskRender::renderingTask() const
+{
+    return renderingTask_;
+}
+
+const Transformation *TaskRender::transformation() const
+{
+    return transformation_;
+}
+
+void TaskRender::run()
+{
+    switch ( renderingTask_ )
+    {
+    case RenderingTask::InitialRendering :
+    {
+        node_.initialRendering();
+        break;
+    }
+    case RenderingTask::ApplyTransformation:
+    {
+        node_.applyTransformation( transformation_ );
+        break;
+    }
+
+    }
+
+}
--- TaskRender.h
+++ TaskRender.h
@@ -1,12 +1,35 @@
 #ifndef TASKRENDER_H
 #define TASKRENDER_H
 
-#include <QThreadPool>
+#include <QRunnable>
+#include <QObject>
+#include <VirtualNode.h>
+#include <Transformation.h>
 
-class TaskRender : public QObject , public QThreadPool
+class TaskRender : public QObject , public QRunnable
 {
 public:
-    TaskRender();
+    enum RenderingTask { InitialRendering , ApplyTransformation } ;
+
+    TaskRender( const VirtualNode &node ,
+                const RenderingTask renderingTask = RenderingTask::InitialRendering ,
+                const Transformation *transformation = nullptr );
+
+    const VirtualNode &node() const ;
+    RenderingTask renderingTask() const;
+    const Transformation *transformation() const;
+
+    void run();
+
+signals :
+    finishedRendering( TaskRender *thisPtr ) ;
+
+
+private:
+    const VirtualNode &node_;
+    const RenderingTask renderingTask_;
+    const Transformation *transformation_;
+
 };
 
 #endif // TASKRENDER_H
