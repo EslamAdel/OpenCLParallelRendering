69f2447d44daa7a817249c31ee2bda00aaea7f55
Asem Abdelaziz <asem_alla@msn.com>
2016-04-07 15:13:01 +0200

    Add missing header file to CMake

--- src/framework/Transformation.h
+++ src/framework/Transformation.h
@@ -5,14 +5,47 @@
 struct Transformation
 {
     Transformation();
+
+
     //Transformation parameters
+    /**
+     * @brief rotation
+     */
     Coordinates3D rotation ;
+
+    /**
+     * @brief translation
+     */
     Coordinates3D translation ;
+
+    /**
+     * @brief scale
+     */
     Coordinates3D scale ;
+
+    /**
+     * @brief brightness
+     */
     float brightness ;
+
+    /**
+     * @brief volumeDensity
+     */
     float volumeDensity ;
+
+    /**
+     * @brief transferFunctionFlag
+     */
     int transferFunctionFlag ;
+
+    /**
+     * @brief transferFunctionScale
+     */
     float transferFunctionScale ;
+
+    /**
+     * @brief transferFunctionOffset
+     */
     float transferFunctionOffset ;
 
 };
